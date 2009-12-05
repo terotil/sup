d4abfed33d5a001cb35296f23819494fbb08cf28
Rich Lane <rlane@club.cc.cmu.edu>
2009-12-04 18:55:36 -0800

    storage

---
+++ lib/sup/storage.rb
@@ -0,0 +1,59 @@
+require 'thread'
+require 'zlib'
+
+module Redwood
+
+class Storage
+	LEVEL = Zlib::BEST_SPEED
+	FOOTER_SIZE = 8
+	FOOTER_FMT = 'NN'
+
+	def initialize fn
+		exists = File.exists? fn
+
+		@io = if exists
+			File.new(fn, 'r+b')
+		else
+			File.new(fn, 'w+b')
+		end
+
+		@length = @io.stat.size
+		@lock = Mutex.new
+	end
+
+	def put data
+		fail 'closed' if @io.closed?
+		zdata = Zlib::Deflate.deflate data, LEVEL
+		zsize = zdata.bytesize
+		@lock.synchronize do
+			offset = @io.pos = @length
+			@io.write zdata
+			offset += zsize
+			@io.write [mkhash(offset), zsize].pack(FOOTER_FMT)
+			@length = offset + FOOTER_SIZE
+			offset
+		end
+	end
+
+	def get offset
+		fail 'closed' if @io.closed?
+		zdata = @lock.synchronize do
+			@io.pos = offset
+			magic, zsize, = @io.read(FOOTER_SIZE).unpack(FOOTER_FMT)
+			fail "bad magic" unless magic == mkhash(offset)
+			@io.pos = offset - zsize
+			@io.read zsize
+		end
+		Zlib::Inflate.inflate zdata
+	end
+
+	def close
+		@io.close
+	end
+
+	def mkhash x
+		(x * 2654435761) & 0xFFFFFFFF
+	end
+end
+
+end
---
+++ test/test_storage.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'sup/storage'
+require 'stringio'
+require 'tmpdir'
+require 'fileutils'
+
+class TestStorage < Test::Unit::TestCase
+  def setup
+    @path = Dir.mktmpdir
+  end
+
+  def teardown
+    FileUtils.rm_r @path
+  end
+
+  def with_store fn='db', &b
+    store = Redwood::Storage.new File.join(@path, fn)
+    ret = b.yield store
+    store.close
+    ret
+  end
+
+  def test_simple
+    data = [
+      'foo',
+      'bar---',
+      "\000\n\b\abop",
+      'zap+',
+    ]
+    data.each { |d| d.force_encoding Encoding::ASCII_8BIT }
+
+    offsets = []
+
+    with_store do |store|
+      data.each do |d|
+        assert d.valid_encoding?
+        offsets << store.put(d)
+        offsets.zip(data).each do |o2,d2|
+          d3 = store.get o2
+          assert d3.valid_encoding?
+          assert_equal d2, d3
+        end
+      end
+
+      data.zip(offsets).shuffle.each do |d,o|
+        d2 = store.get o
+        assert_equal d, d2
+      end
+    end
+  end
+
+  def test_reopen
+    d = 'x'
+    o = with_store { |store| store.put d }
+    x = with_store { |store| store.get o }
+    assert_equal d, x
+  end
+end
