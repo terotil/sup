41961480da45d3a0944f7746a1cb19e777627e2f
Tero Tilus <tero@tilus.net>
2010-01-21 23:12:57 +0200

    Added logging
     - debug-log reply-from selection
     - sup-sync emits message ids when output is verbose
    
    Signed-off-by: Tero Tilus <tero@tilus.net>

--- lib/sup/modes/reply-mode.rb
+++ lib/sup/modes/reply-mode.rb
@@ -63,7 +63,13 @@ EOS
     ## determine the from address of a reply.
     ## if we have a value from a hook, use it.
     from = if hook_reply_from
+      debug "Replying from #{hook_reply_from} set by reply-from hook"
       hook_reply_from
+    ## otherwise, try and find an account somewhere in the list of to's
+    ## and cc's.
+    elsif(b = (@m.to + @m.cc).find { |p| AccountManager.is_account? p })
+      debug "Replying from #{b.email} found on recipient headers"
+      b
     ## otherwise, if the original email had an envelope-to header, try and use
     ## it, and look up the corresponding name form the list of accounts.
     ##
@@ -71,13 +77,11 @@ EOS
     ## To: is the list id itself). if the user subscribes via a particular
     ## alias, we want to use that alias in the reply.
     elsif @m.recipient_email && (a = AccountManager.account_for(@m.recipient_email))
+      debug "Replying from #{a.email} matching #{@m.recipient_email} found on envelope"
       Person.new a.name, @m.recipient_email
-    ## otherwise, try and find an account somewhere in the list of to's
-    ## and cc's.
-    elsif(b = (@m.to + @m.cc).find { |p| AccountManager.is_account? p })
-      b
     ## if all else fails, use the default
     else
+      debug "Replying from default account"
       AccountManager.default_account
     end
 
